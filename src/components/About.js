6a1203a1dbcc6a0dfed4a9ba2b30f263647e7b65
sumandhungana28 <80553482+sumandhungana28@users.noreply.github.com>
2023-01-12 17:23:38 -0800

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function  About () {
+  return (
+    <div> About the restaurant</div>
+  )
+}
+
+export default  About;
---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Order() {
+  return (
+    <div>
+      this is order page 
+    </div>
+  )
+}
+
+export default Order;
